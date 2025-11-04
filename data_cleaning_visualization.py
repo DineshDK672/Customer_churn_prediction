b7fc8c80d12fcef55f55a6674ebd894f5515835a
Dinesh Kumar <dineshdk672000@gmail.com>
2025-11-04 21:33:55 +0530

    Data Cleaning Commit 1

---
+++ data_cleaning_visualization.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+# To display all the columns without truncating
+pd.set_option('display.max_columns', None)
+
+df = pd.read_csv(
+    "Customer_churn_prediction/WA_Fn-UseC_-Telco-Customer-Churn.csv")
+df = df.drop(['customerID'], axis=1)
+# print(df.head())
+
+# Change 'TotalCharges' to numeric values
+df['TotalCharges'] = pd.to_numeric(df.TotalCharges, errors='coerce')
+print(df.isna().sum())  # To check for null values
+
+# To see the rows with null values in 'TotalCharges'
+print(df[np.isnan(df['TotalCharges'])])
+
+print(df[df['tenure'] == 0].index)  # To check for rows with 'tenure' 0
+
+df.drop(labels=df[df['tenure'] == 0].index, axis=0, inplace=True)
+# There should be no more null values in 'TotalCharges'
+
+print(df[np.isnan(df['TotalCharges'])])
+
+#   To convert numeric values in 'SeniorCitizen' to 'Yes'/'No'
+print(df['SeniorCitizen'].unique())  # To find unique values in 'SeniorCitizen'
+df["SeniorCitizen"] = df['SeniorCitizen'].map({0: 'No', 1: 'Yes'})
+print(df['SeniorCitizen'].unique())
+
+print(df.dtypes)
+
+# Data Visualization
+
+g_labels = df['gender'].unique()
+c_labels = df['Churn'].unique()
+fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(12, 6))
+
+# Gender Chart
+wedges1, texts1, autotexts1 = ax1.pie(df['gender'].value_counts(
+), labels=g_labels, autopct='%1.1f%%', startangle=90)
+ax1.set_title("Chart 1: Gender")
+ax1.axis('equal')
+ax1.legend(wedges1, g_labels, title="Genders")
+
+# Churn Chart
+wedges2, texts2, autotexts2 = ax2.pie(df['Churn'].value_counts(), labels=c_labels,
+                                      autopct='%1.1f%%', startangle=180)
+ax2.set_title("Chart 2: Churn %")
+ax2.axis('equal')
+ax2.legend(wedges2, c_labels, title="Churned")
+
+plt.tight_layout()
+plt.show()
